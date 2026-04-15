de7ab7770abddb2ab20dabb4c485825628d8cbf3
navanath-latakegit config --global user.name navanath-latakegit config --global user.name navanath-latake <navanath.latake01@gmail.com>
2026-04-15 17:29:56 +0530

    Add Profile table for storing user bio, location, and birth date

--- myProject16/myProject16/portfolio/models.py
+++ myProject16/myProject16/portfolio/models.py
@@ -8,3 +8,11 @@ class Student(models.Model):
     
     def __str__(self):
         return self.name
+
+class Profile(models.Model):
+    bio = models.TextField()
+    location = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.location
